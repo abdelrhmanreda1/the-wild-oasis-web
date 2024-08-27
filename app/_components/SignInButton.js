80917e8efb2babc8149d5790d3f1a627a1e8fc9f
abdelrahman <abdelrhamanreda11@gmail.com>
2024-08-27 11:56:22 +0300

    edit login page

--- app/_components/SignInButton.js
+++ app/_components/SignInButton.js
@@ -3,9 +3,9 @@ import { signInAction } from "@/app/_libs/actions";
 function SignInButton() {
   return (
     <form action={signInAction}>
-      <button className="flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium hover:border-primary-600 hover:text-primary-200">
+      <button className="flex items-center sm:gap-6 gap-3 text-lg border border-primary-300 px-10 py-4 font-medium hover:border-primary-600 hover:text-primary-200">
         <img src="https://authjs.dev/img/providers/google.svg" alt="Google logo" height="24" width="24" />
-        <span>Continue with Google</span>
+        <span className="text-sm sm:text-lg">Continue with Google</span>
       </button>
     </form>
   );
